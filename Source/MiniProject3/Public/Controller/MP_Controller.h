1c83c500b45335911fa6d39300973b0007ce3a68
Julien soum <julienssmm@gmail.com>
2025-11-18 10:30:47 +0100

    Add Basic movements

---
+++ Source/MiniProject3/Public/Controller/MP_Controller.h
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MP_Controller.generated.h"
+
+class UInputMappingContext;
+struct FInputActionValue;
+class UInputAction;
+class UCharacterMovementComponent;
+class AMP_BaseCharacter;
+/**
+ * 
+ */
+UCLASS(Abstract)
+class MINIPROJECT3_API AMP_Controller : public APlayerController
+{
+    GENERATED_BODY()
+
+public:
+    // Movement
+    virtual void AddPitchInput(float Val) override;
+    virtual void AddYawInput(float Val) override;
+
+
+protected:
+    // Overrides
+    virtual void SetupInputComponent() override;
+    virtual void SetPawn(APawn* InPawn) override;
+
+    // Movement
+    void MovePlayer(const FInputActionValue& Value);
+    void TurnPlayer(const FInputActionValue& Value);
+
+    void StartSprintPlayer(const FInputActionValue& Value);
+    void StopSprintPlayer(const FInputActionValue& Value);
+    
+    void StartJumpPlayer(const FInputActionValue& Value);
+    void StopJumpPlayer(const FInputActionValue& Value);
+
+
+    // Inputs
+    UPROPERTY(EditDefaultsOnly, Category = "EnhancedInput")
+    TObjectPtr<UInputMappingContext> InputMappingContext;
+
+    UPROPERTY(EditDefaultsOnly, Category = "EnhancedInput")
+    TObjectPtr<UInputAction> InputActionMove;
+
+    UPROPERTY(EditDefaultsOnly, Category = "EnhancedInput")
+    TObjectPtr<UInputAction> InputActionTurn;
+
+    UPROPERTY(EditDefaultsOnly, Category = "EnhancedInput")
+    TObjectPtr<UInputAction> InputActionSprint;
+
+    UPROPERTY(EditDefaultsOnly, Category = "EnhancedInput")
+    TObjectPtr<UInputAction> InputActionJump;
+
+    TWeakObjectPtr<AMP_BaseCharacter> Character;
+    TWeakObjectPtr<UCharacterMovementComponent> CharacterMovementComponent;
+    // TWeakObjectPtr<UCC_GravityGunController> GravityGunController;
+    // TWeakObjectPtr<UScoreComponentController> ScoreController;
+    // TWeakObjectPtr<UCC_PickupSpawnerController> PickupSpawner;
+    // TWeakObjectPtr<UCC_PickupCountController> PickupCount;
+
+    UPROPERTY(EditDefaultsOnly, Category = "EnhancedInput|Mouse", meta = (ClampMin = 0.1f, ClampMax = 100.0f))
+    float SensitivityX = 0.8f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Settings|Mouse", meta = (ClampMin = 0.1f, ClampMax = 100.0f))
+    float SensitivityY = 0.8f;
+
+};
---
+++ Source/MiniProject3/Public/Gameplay/MP_BaseCharacter.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MP_BaseCharacter.generated.h"
+
+UCLASS()
+class MINIPROJECT3_API AMP_BaseCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    AMP_BaseCharacter();
+};
